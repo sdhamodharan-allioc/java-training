5c6ecb5c767c287f2ea0f811fcbf12e8eec118ca
Savithri Dhamodharan <sdhamodharan@allioc.com>
2023-03-02 17:57:05 -0500

    Factorial,Reverse with Array,Reverse without Array

---
+++ src/main/java/Sample.java
@@ -0,0 +1,11 @@
+public class Sample {
+    public static void main(String[] args) {
+        {
+            int a,b,c;
+            a=10;
+            b=10;
+            c=a+b;
+            System.out.println("addition:" +c);
+        }
+    }
+}
